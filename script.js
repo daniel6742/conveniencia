79940c35b0cd035b04e14ee6b20e1979148e0087
daniel6742 <matozodaniel7@gmail.com>
2026-07-23 19:31:27 -0400

    Atualização do sistema V2

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const c = document.getElementById("mesas");
 let lucro = 0,
-  p = 0;
+  c = document.getElementById("mesas");
 for (let i = 1; i <= 10; i++) {
-  let d = document.createElement("div");
-  d.className = "mesa";
-  d.innerHTML = `<h3>Mesa ${i}</h3><input placeholder='Cliente'><textarea placeholder='Pedido'></textarea><input type='number' placeholder='Valor'><button>Salvar</button>`;
-  d.querySelector("button").onclick = () => {
-    let v = Number(d.querySelectorAll("input")[1].value) || 0;
-    lucro += v;
-    p++;
-    document.getElementById("l").textContent = lucro.toFixed(2);
-    document.getElementById("p").textContent = p;
-    alert("Pedido salvo!");
+  let tot = 0,
+    d = document.createElement("div");
+  d.className = "mesa livre";
+  d.innerHTML = `<h3>Mesa ${i}</h3><div class=s>Status: Livre</div><input class=v type=number placeholder='Valor'><div>Total:R$ <span class=t>0.00</span></div><button class=a>Abrir</button><button class=g>Pagamento</button><button class=f>Fechar</button>`;
+  d.querySelector(".a").onclick = () => {
+    d.className = "mesa ocupada";
+    d.querySelector(".s").textContent = "Status: Ocupada";
+    tot += Number(d.querySelector(".v").value) || 0;
+    d.querySelector(".t").textContent = tot.toFixed(2);
+  };
+  d.querySelector(".g").onclick = () => {
+    d.className = "mesa pagamento";
+    d.querySelector(".s").textContent = "Status: Aguardando pagamento";
+  };
+  d.querySelector(".f").onclick = () => {
+    lucro += tot;
+    document.getElementById("lucro").textContent = lucro.toFixed(2);
+    tot = 0;
+    d.querySelector(".t").textContent = "0.00";
+    d.querySelector(".v").value = "";
+    d.className = "mesa livre";
+    d.querySelector(".s").textContent = "Status: Livre";
   };
   c.appendChild(d);
 }
